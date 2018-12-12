e5169a81fd70cbd53e7420b21a2df271798e4c55
danielG <greenspan1993@gmail.com>
2018-12-12 16:50:39 +0200

    for all maan debai

--- experiment.py
+++ experiment.py
@@ -1,5 +1,11 @@
 import torch
 
+import torch.nn as nn
+from torch.utils.data import Dataset
+
+import torch.functional as F
+
+
 class LSTMTagger(nn.Module):
 
     def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size):
@@ -13,7 +19,7 @@ class LSTMTagger(nn.Module):
         self.lstm = nn.LSTM(embedding_dim, hidden_dim)
 
         # The linear layer that maps from hidden state space to tag space
-        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
+        #self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
         self.hidden = self.init_hidden()
 
     def init_hidden(self):
@@ -21,13 +27,44 @@ class LSTMTagger(nn.Module):
         # Refer to the Pytorch documentation to see exactly
         # why they have this dimensionality.
         # The axes semantics are (num_layers, minibatch_size, hidden_dim)
-        return (torch.zeros(1, 1, self.hidden_dim),
-                torch.zeros(1, 1, self.hidden_dim))
+        return (torch.zeros(1, 1, self.hidden_dim), torch.zeros(1, 1, self.hidden_dim))
 
     def forward(self, sentence):
         embeds = self.word_embeddings(sentence)
-        lstm_out, self.hidden = self.lstm(
-            embeds.view(len(sentence), 1, -1), self.hidden)
-        tag_space = self.hidden2tag(lstm_out.view(len(sentence), -1))
-        tag_scores = F.log_softmax(tag_space, dim=1)
-        return tag_scores
+        lstm_out, self.hidden = self.lstm(embeds.view(len(sentence), 1, -1), self.hidden)
+        #tag_space = self.hidden2tag(lstm_out.view(len(sentence), -1))
+        #tag_scores = F.log_softmax(tag_space, dim=1)
+        return lstm_out,self.hidden
+
+    data_file, is_train = True, separator = " "):
+    """
+    get data as windows saved in data loader
+    :param data_file: path to file
+    :param is_train: is train routine
+    :param separator: seprator in files
+    :return: data loader
+    """
+    print "Getting data from: ", data_file
+    sentences = utils.read_data(data_file, is_train=True, seperator=separator)
+    if is_train:
+        utils.initialize_indexes()
+    windows, tags = utils.create_windows(sentences)
+    windows, tags = np.asarray(windows, np.float32), np.asarray(tags, np.int32)
+    windows, tags = torch.from_numpy(windows), torch.from_numpy(tags)
+    windows, tags = windows.type(torch.LongTensor), tags.type(torch.LongTensor)
+    dataset = torch.utils.data.TensorDataset(windows, tags)
+    if is_train:
+        return DataLoader(dataset, batch_size=BATCH, shuffle=True)
+    return DataLoader(dataset, batch_size=1, shuffle=True)
+
+
+class utils:
+
+
+
+
+    C2I = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6,
+           "7": 7, "8": 8, "9": 9, "a": 10, "b": 11, "c": 12, "d": 13}
+
+
+
