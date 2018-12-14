fef14af5a9d11adc5f62f8726016ad5deb771df1
eilon47 <33837343+eilon47@users.noreply.github.com>
2018-12-14 11:29:19 +0200

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -10,7 +10,9 @@ STUDENT={'name': 'Daniel Greenspan_Eilon Bashari',
          'ID': '308243948_308576933'}
 
 # Globals
-IN_DIM = 100
+LAYERS = 1
+EMBEDDED = 50
+IN_DIM = 150
 HID_DIM = 100
 TAGS_SIZE = 2
 EPOCHS = 3
@@ -18,37 +20,58 @@ VSIZE = len(C2I)
 
 
 class LSTM(object):
-    def __init__(self, in_dim, lstm_dim, out_dim, model):
-        self.builder = dy.VanillaLSTMBuilder(1, in_dim, lstm_dim, model)
-        self.W = model.add_parameters((out_dim, lstm_dim))
+    def __init__(self,layers, em_dim, in_dim, hid_dim, out_dim, vsize, model):
+        self.__RNN = dy.LSTMBuilder(layers, em_dim, in_dim, model)
+        self.__E = model.add_lookup_parameters((vsize, em_dim))
+        self.__W1 = model.add_parameters((hid_dim, in_dim))
+        self.__b1 = model.add_parameters(hid_dim)
+        self.__W2 = model.add_parameters((out_dim, hid_dim))
+        self.__b2 = model.add_parameters(out_dim)
 
     def __call__(self, sequence):
-        lstm = self.builder.initial_state()
-        prm = self.W.expr()
-        sn= lstm.transduce(sequence)
-        result = prm * sn[-1]
-        return result
+        dy.renew_cg()
+        W1 = dy.parameter(self.__W1)
+        W2 = dy.parameter(self.__W2)
+        b1 = dy.parameter(self.__b1)
+        b2 = dy.parameter(self.__b2)
+        lstm = self.__RNN.initial_state()
+        embedded = [dy.lookup(self.__E, s) for s in sequence]
+        out = lstm.transduce(embedded)
+        x = out[-1]
+        h = dy.tanh(W1*x + b1)
+        return dy.softmax(W2*h + b2)
+
+    def get_loss(self, sequence, expected):
+        probs = self(sequence)
+        return -dy.log(dy.pick(probs, expected))
+
+    def predict(self, sequence):
+        probs = self(sequence)
+        return np.argmax(probs.npvalue())
 
 
 class Trainer(object):
     def __init__(self):
         self.m = dy.Model()
         self.trainer = dy.AdamTrainer(self.m)
-        self.E = self.m.add_lookup_parameters((VSIZE, IN_DIM))
-        self.acceptor = LSTM(IN_DIM, HID_DIM, TAGS_SIZE, self.m)
+        self.acceptor = LSTM(layers=LAYERS, in_dim=IN_DIM, hid_dim=HID_DIM,
+                             out_dim=TAGS_SIZE, em_dim=EMBEDDED, vsize=VSIZE, model=self.m)
 
-    def train(self, train_data, test_data):
-        sum_loss = 0.0
+    def train(self, train_data, dev_data):
         start_time = time.time()
         for epoch in range(EPOCHS):
-            print "Epoch number ", epoch +1, " started!"
-            random.shuffle(train_data)
-            sum_loss += self.routine(train_data, is_train=True)
-            print "train #{}: loss is {}, accuracy is {}%".format(epoch, sum_loss/len(train_data), self.get_accuracy(train_data))
-
-            test_loss = self.routine(test_data)
-            print "test #{}: loss is {}, accuracy is {}%".format(epoch, test_loss/len(test_data), self.get_accuracy(test_data))
             sum_loss = 0.0
+            random.shuffle(train_data)
+            print "Epoch number ", epoch, " started!"
+            for word, tag in train_data:
+                vector = self.word_to_vec(word)
+                loss = self.acceptor.get_loss(vector, T2I[tag])
+                sum_loss += loss.value()
+                loss.backward()
+                self.trainer.update()
+            avg_loss = sum_loss/len(train_data)
+            accuracy = self.get_accuracy(dev_data)
+            print "train #{}: loss is {}, accuracy is {}%".format(epoch, avg_loss, accuracy)
 
         end_time = time.time()
         total_time = end_time - start_time
@@ -67,24 +90,18 @@ class Trainer(object):
                 self.trainer.update()
         return sum_loss
 
-    def predict(self, word):
-        dy.renew_cg()
-        vec = self.word_to_vec(word)
-        preds = dy.softmax(self.acceptor(vec))
-        vals = preds.npvalue()
-        return np.argmax(vals)
-
     def get_accuracy(self, data):
         good = 0
+        random.shuffle(data)
         for word,tag in data:
-            pred = self.predict(word)
-            if T2I[tag] == pred:
+            sequence = self.word_to_vec(word)
+            pred = self.acceptor.predict(sequence)
+            if pred == T2I[tag]:
                 good += 1
-        print good, len(data)
         return 100*(float(good) / float(len(data)))
 
     def word_to_vec(self, word):
-        return [self.E[C2I[c]] for c in word]
+        return [C2I[c] for c in word]
 
 
 def main(argv):
