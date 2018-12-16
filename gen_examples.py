199f2ca83947271d500166eced9b8bdb5b238d33
danielG <greenspan1993@gmail.com>
2018-12-16 15:23:00 +0200

    part 3

--- gen_examples.py
+++ gen_examples.py
@@ -32,6 +32,47 @@ def generate_seq(num):
 
     return positive,negative
 
+def generate_pol(num):
+    words = []
+    for i in range(num):
+        pos_num_seq = "".join([random.choice("0123456789abcdefghigklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+                               for x in range(random.randint(1,30))])
+        rev = pos_num_seq[::-1]
+        word = pos_num_seq + '#' + rev
+        words.append(word)
+    with open('polindrom_examples','w+') as f:
+        for word in words:
+            f.write(word +'\n')
+    return words
+
+def generate_double_word(num):
+    words = []
+    for i in range(num):
+        # gets new number for positive seq.
+        pos_num_seq = "".join([random.choice("0123456789abcdefghigklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+                               for x in range(random.randint(1,30))])
+        rev = pos_num_seq
+        word = pos_num_seq + rev
+        words.append(word)
+    with open('double_word_examples','w+') as f:
+        for word in words:
+            f.write(word +'\n')
+    return words
+
+def generate_mod_five(num):
+    words = []
+    i = 0
+    while i < num:
+        # gets new number for positive seq.
+        word = "".join([random.choice("0123456789") for x in range(random.randint(1,30))])
+        if int(word) % 5 == 0:
+            words.append(str(word))
+            i += 1
+    with open('mod_examples','w+') as f:
+        for word in words:
+            f.write(word +'\n')
+    return words
+
 
 def write_examples(pos,neg,num):
     pos_fd = open('pos_examples', 'w')
@@ -41,7 +82,8 @@ def write_examples(pos,neg,num):
         neg_fd.write(neg[k] + '\n')
     pos_fd.close()
     neg_fd.close()
-    print 'successfully created 500 examples!'
+    print ('successfully created 500 examples!')
+
 
 
 def write_train(pos,neg,num):
@@ -58,10 +100,10 @@ def write_train(pos,neg,num):
             neg_count += 1
     fd.close()
     if any([pos_count/num > 0.7, neg_count/num > 0.7]):
-        print 'trying again!'
+        print ('trying again!')
         write_train(pos,neg,num)
     else:
-        print 'successfully created the train set!'
+        print ('successfully created the train set!')
 
 
 def write_dev(pos,neg,num):
@@ -75,13 +117,14 @@ def write_dev(pos,neg,num):
             pos_count += 1
         else:
             fd.write(neg[k] + '\tneg\n')
+
             neg_count += 1
     fd.close()
     if any([pos_count/num > 0.7, neg_count/num > 0.7]):
-        print 'trying again!'
+        print ('trying again!')
         write_train(pos,neg,num)
     else:
-        print 'successfully created the test set!'
+        print ('successfully created the test set!')
 
 
 def write_test(pos,neg,num):
@@ -98,12 +141,16 @@ def write_test(pos,neg,num):
             neg_count += 1
     fd.close()
     if any([pos_count / num > 0.7, neg_count / num > 0.7]):
-        print 'trying again!'
+        print ('trying again!')
         write_train(pos, neg, num)
     else:
-        print 'successfully created the test set!'
+        print ('successfully created the test set!')
 
 if __name__ == '__main__':
+    words = generate_pol(500)
+    words2 = generate_double_word(500)
+    words3 = generate_mod_five(500)
+
     pos_ex,neg_ex = generate_seq(500)
     write_examples(pos_ex,neg_ex,500)
 
@@ -112,5 +159,4 @@ if __name__ == '__main__':
 
     pos_dev, neg_dev = generate_seq(2000)
     write_dev(pos_dev, neg_dev, 2000)
-
 
